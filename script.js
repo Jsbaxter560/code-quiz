89784a39fc4ee174e573b0c210632d0b28558b2b
John Baxter <juancho420@ymail.com>
2021-03-28 14:51:45 -0700

    initial pseudocode attempt

---
+++ script.js
@@ -0,0 +1,14 @@
+// User is presented with a window inviting them to begin code quiz by clicking start button
+// button includes addEventListener awaiting click
+// click initiates var quiz
+// quiz includes timer utilizing setInterval function
+// quiz includes questions drawn from an array of strings
+// quiz includes answers drawn from an array of strings
+// as each question is displayed 4 answers are displayed
+// for each question, only one answer is true
+// for each false answer, 10 seconds are decremented from remaining time
+// for each true answer, time remaining is left alone
+// if all questions are answered game is over
+// if time remaining reaches 0 game is over
+// if game is over user is presented with a prompt to enter initials alongside amount of time remaining
+// when initials are input, they are displayed on a list of high scores in descending order
