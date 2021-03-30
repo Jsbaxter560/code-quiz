7be629dbc9d3dfd0ff5339cb58efdbd4a8554e76
John Baxter <juancho420@ymail.com>
2021-03-29 23:23:15 -0700

    further wrinkles added to pseudocode with classes in html

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // User is presented with a window inviting them to begin code quiz by clicking start button
+    // 
 // button includes addEventListener awaiting click
-// click initiates var quiz
+// click initiates function quiz
 // quiz includes timer utilizing setInterval function
+    //timer require var clock and var countdown
+    // timer includes setTimeout method to end quiz
 // quiz includes questions drawn from an array of strings
 // quiz includes answers drawn from an array of strings
 // as each question is displayed 4 answers are displayed
@@ -12,3 +15,7 @@
 // if time remaining reaches 0 game is over
 // if game is over user is presented with a prompt to enter initials alongside amount of time remaining
 // when initials are input, they are displayed on a list of high scores in descending order
+
+var answers =["variable", "syntax", "function", "array", "method", "concatenate", "pseudocode", "index",];
+
+var questions =["'_____' refers to the rules by which a programming language functions", "It is best to '__________' at the beginning of any project to establish a path"];
